ec0207376ba40fb50b7cc608a42971356411faa5
Luis Mendoza <luis29798@gmail.com>
2023-06-28 17:14:17 -0400

    CSV version

--- models/shortURL.js
+++ models/shortURL.js
@@ -1,21 +1,60 @@
-const mongoose = require('mongoose')
-const shortId = require('shortid')
+const fs = require('fs');
+const shortId = require('shortid');
 
-const shortUrlSchema = new mongoose.Schema({
-  full: {
-    type: String,
-    required: true
-  },
-  short: {
-    type: String,
-    required: true,
-    default: shortId.generate
-  },
-  clicks: {
-    type: Number,
-    required: true,
-    default: 0
-  }
-})
+// Function to save a short URL to the CSV file
+function saveShortUrl(shortUrl) {
+  fs.readFile('shorturls.csv', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading short URLs:', err);
+      return;
+    }
 
-module.exports = mongoose.model('ShortUrl', shortUrlSchema)
+    const rows = data.trim().split('\n');
+    const existingUrls = rows.map((row) => {
+      const [full] = row.split(',');
+      return full;
+    });
+
+    if (existingUrls.includes(shortUrl.full)) {
+      console.log('Short URL already exists:', shortUrl.full);
+      return;
+    }
+
+    const csvRow = `${shortUrl.full},${shortUrl.short},${shortUrl.clicks}\n`;
+
+    fs.appendFile('shorturls.csv', csvRow, { flag: 'a+' }, (err) => {
+      if (err) {
+        console.error('Error saving short URL:', err);
+      } else {
+        console.log('Short URL saved successfully!');
+      }
+    });
+  });
+}
+
+// Create the CSV file if it doesn't exist
+function createCSVFile() {
+  fs.open('shorturls.csv', 'a+', (err) => {
+    if (err) {
+      console.error('Error creating CSV file:', err);
+    } else {
+      console.log('CSV file created successfully!');
+    }
+  });
+}
+
+// Check if the CSV file exists and create it if necessary
+function initialize() {
+  fs.access('shorturls.csv', fs.constants.F_OK, (err) => {
+    if (err) {
+      createCSVFile();
+    }
+  });
+}
+
+// Initialize the short URL system
+initialize();
+
+module.exports = {
+  saveShortUrl,
+};
--- server.js
+++ server.js
@@ -1,34 +1,83 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortURL = require('./models/shortURL')
-const app = express()
+const express = require('express');
+const fs = require('fs');
+const shortId = require('shortid'); 
+const ShortUrl = require('./models/shortUrl');
+const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost/urlShortener', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+// Function to save a short URL to the CSV file
+function saveShortUrl(shortUrl) {
+  const csvRow = `${shortUrl.full},${shortUrl.short},${shortUrl.clicks}\n`;
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+  fs.appendFile('shorturls.csv', csvRow, { flag: 'a+' }, (err) => {
+    if (err) {
+      console.error('Error saving short URL:', err);
+    } else {
+      console.log('Short URL saved successfully!');
+    }
+  });
+}
 
 app.get('/', async (req, res) => {
-    const shortURLs = await ShortURL.find()
-    res.render('index', {shortURLs: shortURLs})
-})
+  fs.readFile('shorturls.csv', 'utf8', async (err, data) => {
+    if (err) {
+      console.error('Error reading short URLs:', err);
+      return res.sendStatus(500);
+    }
 
-app.post("/shortURLs", async (req, res) => {
-   await ShortURL.create({full: req.body.fullURL})
-   res.redirect('/')
-})
+    const rows = data.trim().split('\n');
+    const shortUrls = rows.map((row) => {
+      const [full, short, clicks] = row.split(',');
+      return {
+        full,
+        short,
+        clicks: parseInt(clicks),
+      };
+    });
 
-app.get('/shortURL', async (req, res) => {
-    const shortURL = ShortURL.findOne({short: req.params.ShortURL})
-    if (shortURL == null) return res.sendStatus(404)
+    res.render('index', { shortUrls });
+  });
+});
 
-    shortURL.clicks++
-    shortURL.save()
+app.post('/shortUrls', async (req, res) => {
+  const newShortUrl = {
+    full: req.body.fullUrl,
+    short: req.body.customShortUrl || shortId.generate(),
+    clicks: 0,
+  };
 
-    res.redirect(shortURL.full)
-})
+  saveShortUrl(newShortUrl);
 
-app.listen(process.env.PORT || 5005)
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  fs.readFile('shorturls.csv', 'utf8', async (err, data) => {
+    if (err) {
+      console.error('Error reading short URLs:', err);
+      return res.sendStatus(500);
+    }
+
+    const rows = data.trim().split('\n');
+    const shortUrls = rows.map((row) => {
+      const [full, short, clicks] = row.split(',');
+      return {
+        full,
+        short,
+        clicks: parseInt(clicks),
+      };
+    });
+
+    const shortUrl = shortUrls.find((url) => url.short === req.params.shortUrl);
+    if (shortUrl == null) return res.sendStatus(404);
+
+    shortUrl.clicks++;
+    saveShortUrl(shortUrl);
+
+    res.redirect(shortUrl.full);
+  });
+});
+
+app.listen(process.env.PORT || 5005);
